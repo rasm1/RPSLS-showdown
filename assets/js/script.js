b91bcf460a7b8c20673ad2dea5c2b6647d2240eb
Mees R. <rasmees123@gmail.com>
2024-06-24 18:31:56 +0000

    Add script for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,33 @@
-function runGame(){
+const buttons = document.getElementsByClassName('game-options');
+const playerScore = document.getElementById('playerScore');
+const computerScore = document.getElementById('computerScore');
+const choices = ['Rock', 'Paper', 'Scissors', 'lizard', 'spock'];
+
+/**
+ * loops through buttons and adds event listener to each
+ */
+for(let button of buttons){
+    button.addEventListener('click', function(){
+        let playerChoice = this.getAttribute("data-choice");
+        runGame(playerChoice);
+    });
+}
+function runGame(playerChoice) {
+    let computerChoice = choices[Math.floor(Math.random() * 5)];
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+    incrementScores(result);
+}
+
+/**
+ * checks to see who won
+ */
+function checkWinner(choices[playerChoice], choices[computerChoice]) {
+    if
 
 }
 
-function checkUserAnswer(){
 
-}
 
-function checkComputerAnswer(){
+function incrementScores() {
 
-}
-
-function incrementUserScore(){
-
-}
-
-function incrementComputerScore(){
-    
 }
