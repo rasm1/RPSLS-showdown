a3e62af8ecdf492cbb92930e6cdc106a65ff7353
Mees R. <rasmees123@gmail.com>
2024-06-25 15:27:42 +0000

    Add main game loop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,56 @@
-const buttons = document.getElementsByClassName('game-options');
+const buttons = document.getElementsByClassName('game-option');
 const playerScore = document.getElementById('playerScore');
 const computerScore = document.getElementById('computerScore');
-const choices = ['Rock', 'Paper', 'Scissors', 'lizard', 'spock'];
+const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+
+
 
 /**
  * loops through buttons and adds event listener to each
  */
-for(let button of buttons){
-    button.addEventListener('click', function(){
+
+for (let button of buttons) {
+    button.addEventListener('click', function () {
         let playerChoice = this.getAttribute("data-choice");
         runGame(playerChoice);
     });
 }
+
 function runGame(playerChoice) {
     let computerChoice = choices[Math.floor(Math.random() * 5)];
-    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
-    incrementScores(result);
+    checkWinner(playerChoice, computerChoice);
+
 }
 
+function checkWinner(playerChoice, computerChoice) {
+    let result = '';
+    if (playerChoice === computerChoice) {
+        result = 'TIED!';
+    } else {
+        switch (playerChoice) {
+            case 'rock':
+                result = (computerChoice === 'scissors' || computerChoice ==='lizard') ? 'YOU WIN' : "YOU LOSE"
+                break;
+            case 'scissors':
+                result = (computerChoice === 'lizard' || computerChoice ==='paper') ? "YOU WIN" : "YOU LOSE"
+                break;
+            case 'paper':
+                result = (computerChoice === 'rock' || computerChoice ==='spock') ? "YOU WIN" : "YOU LOSE"
+                break;
+            case 'lizard':
+                result = (computerChoice === 'paper' || computerChoice ==='spock') ? "YOU WIN" : "YOU LOSE"
+                break;
+            case 'spock':
+                result = (computerChoice === 'rock' || computerChoice ==='scissors') ? "YOU WIN" : "YOU LOSE"
+        }
+        console.log(result);
+        console.log(playerChoice);
+        console.log(computerChoice);
+
+
+    };
+}
 /**
  * checks to see who won
  */
-function checkWinner(choices[playerChoice], choices[computerChoice]) {
-    if
-
-}
-
-
-
-function incrementScores() {
-
-}
