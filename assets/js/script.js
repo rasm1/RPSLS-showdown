28a2421c3207e1c876141f6957bc9f3ca742af5f
Mees R. <rasmees123@gmail.com>
2024-07-02 10:44:13 +0000

    Add win/lose condition to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,26 +37,26 @@ function checkWinner(playerChoice, computerChoice) {
         resultDisplay.innerHTML = result;
         resultDisplay.classList.remove('green-text', 'red-text');
         return;
-    } 
-        switch (playerChoice) {
-            case 'rock':
-                result = (computerChoice === 'scissors' || computerChoice === 'lizard') ? 'YOU WIN' : "YOU LOSE"
-                break;
-            case 'scissors':
-                result = (computerChoice === 'lizard' || computerChoice === 'paper') ? "YOU WIN" : "YOU LOSE"
-                break;
-            case 'paper':
-                result = (computerChoice === 'rock' || computerChoice === 'spock') ? "YOU WIN" : "YOU LOSE"
-                break;
-            case 'lizard':
-                result = (computerChoice === 'paper' || computerChoice === 'spock') ? "YOU WIN" : "YOU LOSE"
-                break;
-            case 'spock':
-                result = (computerChoice === 'rock' || computerChoice === 'scissors') ? "YOU WIN" : "YOU LOSE"
-        }
+    }
+    switch (playerChoice) {
+        case 'rock':
+            result = (computerChoice === 'scissors' || computerChoice === 'lizard') ? 'YOU WIN' : "YOU LOSE"
+            break;
+        case 'scissors':
+            result = (computerChoice === 'lizard' || computerChoice === 'paper') ? "YOU WIN" : "YOU LOSE"
+            break;
+        case 'paper':
+            result = (computerChoice === 'rock' || computerChoice === 'spock') ? "YOU WIN" : "YOU LOSE"
+            break;
+        case 'lizard':
+            result = (computerChoice === 'paper' || computerChoice === 'spock') ? "YOU WIN" : "YOU LOSE"
+            break;
+        case 'spock':
+            result = (computerChoice === 'rock' || computerChoice === 'scissors') ? "YOU WIN" : "YOU LOSE"
+    }
 
 
-    
+
     /* displays what choice the player and computer make */
     playerDisplay.innerHTML = `you picked: ${playerChoice}`
     computerDisplay.innerHTML = `the computer picked: ${computerChoice}`
@@ -75,5 +75,42 @@ function checkWinner(playerChoice, computerChoice) {
             computerScoreStandard++;
             computerScore.textContent = computerScoreStandard;
             break;
+    }
+    /* modal */
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // get the text inside the modal
+    var modalText = document.getElementById('modalText');
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    if (playerScoreStandard === 15) {
+        modal.style.display = "block";
+    } else if (computerScoreStandard === 15){
+        modalText.textContent ="The computer was one step ahead of you! Try again?";
+        modal.style.display = "block";
+      
+        
     }
 }
